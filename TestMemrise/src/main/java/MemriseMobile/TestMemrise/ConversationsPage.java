40242ca3f8bb54b3727ad7cc893c09dc8e9fd1ae
hannafarhoud <farhoudhanna@gmail.com>
2025-09-18 21:34:46 +0300

    initial commit.

--- TestMemrise/src/main/java/MemriseMobile/TestMemrise/ConversationsPage.java
+++ TestMemrise/src/main/java/MemriseMobile/TestMemrise/ConversationsPage.java
@@ -1,11 +1,5 @@
 package MemriseMobile.TestMemrise;
 
-import org.openqa.selenium.WebElement;
-
-import io.appium.java_client.android.AndroidDriver;
-
 public class ConversationsPage {
-	public static AndroidDriver<WebElement> driver;
-	public int x = 1;
 
 }
