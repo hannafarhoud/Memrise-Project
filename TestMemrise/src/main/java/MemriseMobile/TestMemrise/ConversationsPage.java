ebdb89ffc5a4238fd25a6d6c2f5599a17bff0d69
PH-User <PH-User@DESKTOP-E2CT9PT>
2025-09-18 21:05:23 +0300

    hanna

--- TestMemrise/src/main/java/MemriseMobile/TestMemrise/ConversationsPage.java
+++ TestMemrise/src/main/java/MemriseMobile/TestMemrise/ConversationsPage.java
@@ -1,5 +1,10 @@
 package MemriseMobile.TestMemrise;
 
+import org.openqa.selenium.WebElement;
+
+import io.appium.java_client.android.AndroidDriver;
+
 public class ConversationsPage {
+	public static AndroidDriver<WebElement> driver;
 
 }
