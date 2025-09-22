64663f248fa232208301273d84624d7ecc265ad6
Mai Okasha <94134290+MaiOkasha@users.noreply.github.com>
2025-09-22 20:38:12 +0300

    Final Work

--- TestMemrise/src/test/java/tests/LoginTest.java
+++ TestMemrise/src/test/java/tests/LoginTest.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class LoginTest extends SetupLoginTest {
+public class LoginTest extends TestBase {
 
 	private LoginPage authPage;
 
