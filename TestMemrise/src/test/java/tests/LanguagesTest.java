3c2248e96c1878f75b38cae516a6c836265da2d0
Mai Okasha <94134290+MaiOkasha@users.noreply.github.com>
2025-09-24 03:15:57 +0300

    Add Assertions

--- TestMemrise/src/test/java/tests/LanguagesTest.java
+++ TestMemrise/src/test/java/tests/LanguagesTest.java
@@ -1,9 +1,8 @@
-package tests;
+package MemriseMobile.TestMemrise;
 
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
-import pages.LanguagesPage;
 
 public class LanguagesTest extends TestBase {
 
@@ -12,22 +11,27 @@ public class LanguagesTest extends TestBase {
 	@BeforeClass
 	public void init() {
 		languagesPage = new LanguagesPage(driver);
+
 	}
 
 	@Test(priority = 1)
 	public void testChangeLearningLanguage() {
 		languagesPage.changeLanguageToFrench();
+
 	}
 
 	@Test(priority = 2)
 	public void testAddLanguagesToLearn() {
 		languagesPage.addLanguageToLearn();
 		languagesPage.setUpTheNewLanguage();
+
 	}
 
 	@Test(priority = 3)
 	public void testDeletLanguage() {
 		languagesPage.delteLanguage();
+		boolean isDeleted = driver.findElements(languagesPage.getDeletedLanguageLocator()).size() == 0;
+		Assert.assertTrue(isDeleted);
 	}
 
 }
